5c5082a7f4bac9e1450e189b95c0315bd9910730
thkuo <tku16@helmholtz-hzi.de>
2020-12-14 11:56:07 +0100

    dryrun + installing process-specific env

--- install/INSTALL.sh
+++ install/INSTALL.sh
@@ -1,38 +1,69 @@
+#!/usr/bin/env bash
+export SEQ2GENO_HOME=$( realpath ../ )
+echo 'SEQ2GENO_HOME is '$SEQ2GENO_HOME
+
 ## ensure conda channels
-for c in hzibifo hzi-bifo conda-forge/label/broken bioconda conda-forge defaults; do 
+echo 'check conda channels'
+for c in hzi-bifo conda-forge/label/broken bioconda conda-forge defaults; do 
+	echo $c'...'
 	if [ $(conda config --get channels | grep $c | wc -l) -eq 0 ]; then
 		conda config --add channels $c
 	fi
 done
-### clone seq2geno
-#git clone --recurse-submodules https://github.com/hzi-bifo/seq2geno.git
-#cd seq2geno
-#export SEQ2GENO_HOME=$( realpath . )
-#git submodule update --init --recursive
-#
+cd $SEQ2GENO_HOME
+trap 'echo "Errors in setting conda channels"; exit ' ERR 
+
 ### download the core environment
+#echo 'enter install/'
 #cd install
 #conda env create -n snakemake_env --file=snakemake_env.yml
-#cd ..
+#cd $SEQ2GENO_HOME
+#trap 'echo "Errors in downloading the core environment"; exit ' ERR EXIT 
 #
-### set up environmental variables
-#conda activate snakemake_env
-#cd $CONDA_PREFIX
-#mkdir -p ./etc/conda/activate.d
-#mkdir -p ./etc/conda/deactivate.d
-#export ACTIVATE_ENVVARS=./etc/conda/activate.d/env_vars.sh
-#export DEACTIVATE_ENVVARS=./etc/conda/deactivate.d/env_vars.sh
-#touch $ACTIVATE_ENVVARS
-#touch $DEACTIVATE_ENVVARS
-#
-#echo 'export SEQ2GENO_HOME='$SEQ2GENO_HOME >> $ACTIVATE_ENVVARS
-#echo 'export PATH_BACKUP='$PATH >> $ACTIVATE_ENVVARS
-#echo 'export PATH='$SEQ2GENO_HOME/main:$PATH >> $ACTIVATE_ENVVARS
-#
-#echo 'unset $SEQ2GENO_HOME' >> $DEACTIVATE_ENVVARS
-#echo 'export $PATH=$PATH_BACKUP' >> $DEACTIVATE_ENVVARS
-#echo 'unset $PATH_BACKUP'  >> $DEACTIVATE_ENVVARS
-#conda deactivate
-#
-### decompress the example dataset to install the process-specific environments
+## set up environmental variables
+{ 
+	conda activate snakemake_env
+} || {
+	source activate snakemake_env
+}
 
+echo 'set up core environment'
+echo 'enter '$CONDA_PREFIX
+cd $CONDA_PREFIX
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+export ACTIVATE_ENVVARS=./etc/conda/activate.d/env_vars.sh
+export DEACTIVATE_ENVVARS=./etc/conda/deactivate.d/env_vars.sh
+touch $ACTIVATE_ENVVARS
+touch $DEACTIVATE_ENVVARS
+
+echo 'export SEQ2GENO_HOME='$SEQ2GENO_HOME > $ACTIVATE_ENVVARS
+echo 'export PATH_BACKUP='$PATH >> $ACTIVATE_ENVVARS
+echo 'export PATH='$SEQ2GENO_HOME/main:$PATH >> $ACTIVATE_ENVVARS
+
+echo 'unset SEQ2GENO_HOME' > $DEACTIVATE_ENVVARS
+echo 'export PATH=$PATH_BACKUP' >> $DEACTIVATE_ENVVARS
+echo 'unset PATH_BACKUP'  >> $DEACTIVATE_ENVVARS
+cd $SEQ2GENO_HOME
+trap 'echo "Errors in setting up the core environment"; exit ' ERR 
+
+## Roary
+cd $SEQ2GENO_HOME/denovo/lib/Roary
+./install_dependencies.sh
+conda deactivate
+cd $SEQ2GENO_HOME
+trap 'echo "Errors in installation of Roary dependecies"; exit ' ERR EXIT 
+
+## decompress the example dataset to install the process-specific environments
+echo 'extract example dataset'
+tar -zxvf example_sg_dataset.tar.gz
+cd example_sg_dataset/
+./CONFIG.sh
+seq2geno -f ./seq2geno_inputs.yml
+trap 'echo "Errors in installation of the process-specific environments failed"; exit ' ERR 
+{
+	conda deactivate
+} || {
+	source deactivate
+}
+echo 'Environments installed! You might want to go to example_sg_dataset/ and try. '
