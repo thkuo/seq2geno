924ce807869456b1ef20cd1066e511867dbd8f93
thkuo <tku16@helmholtz-hzi.de>
2020-12-18 19:36:19 +0100

    restore denovo/lib/install_perl_mods.sh

--- denovo/lib/install_perl_mods.sh
+++ denovo/lib/install_perl_mods.sh
@@ -10,17 +10,16 @@
 #cpanm --force Graph
 #cpanm --force Graph::Writer::Dot
 #cpanm --force File::Find::Rule
-#cpanm Bio::Perl
-#cpanm Moose
-#cpanm Log::Log4perl
-#cpanm Array::Utils
-#cpanm File::Grep
-#cpanm File::Which 
-#cpanm File::Slurper
-#cpanm Text::CSV
-#cpanm XML::Parser
-#cpanm Graph
-#cpanm Graph::Writer::Dot
-#cpanm File::Find::Rule
-#cpanm Exception::Class
-cpanm Array::Utils Bio::Perl Exception::Class File::Basename File::Copy File::Find::Rule File::Grep File::Path File::Slurper File::Spec File::Temp File::Which FindBin Getopt::Long Graph Graph::Writer::Dot List::Util Log::Log4perl Moose Moose::Role Text::CSV PerlIO::utf8_strict Devel::OverloadInfo Digest::MD5::File
+cpanm Bio::Perl
+cpanm Moose
+cpanm Log::Log4perl
+cpanm Array::Utils
+cpanm File::Grep
+cpanm File::Which 
+cpanm File::Slurper
+cpanm Text::CSV
+cpanm XML::Parser
+cpanm Graph
+cpanm Graph::Writer::Dot
+cpanm File::Find::Rule
+cpanm Exception::Class
