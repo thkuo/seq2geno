0cacfb311b7c9a52e6c7e8f7851ff6d1fc272d61
thkuo <thkuo@deimos-compute-01.bifo.helmholtz-hzi.de>
2020-12-18 08:51:24 +0100

    PERL5LIB updated by external script

--- denovo/lib/update_perl5lib_for_prokka.sh
+++ denovo/lib/update_perl5lib_for_prokka.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+echo 'current PERL5LIB='$PERL5LIB
 if ! ( echo $PERL5LIB|grep -q $( dirname $( which prokka ) )/../perl5/ ); then
+    echo 'start updating the variable for this environment...'
+    # set up for this time
+    export PERL5LIB_BACKUP=$PERL5LIB
+    export PERL5LIB=$( dirname $( which prokka ) )/../perl5/:$PERL5LIB
+    # automatically set it up next time
     cd $CONDA_PREFIX
     mkdir -p ./etc/conda/activate.d
     mkdir -p ./etc/conda/deactivate.d
@@ -8,7 +14,9 @@ if ! ( echo $PERL5LIB|grep -q $( dirname $( which prokka ) )/../perl5/ ); then
     touch ./etc/conda/deactivate.d/env_vars.sh
     echo 'export PERL5LIB_BACKUP='$PERL5LIB >> ./etc/conda/activate.d/env_vars.sh
     echo 'export PERL5LIB='$( dirname $( which prokka ) )/../perl5/:$PERL5LIB >> ./etc/conda/activate.d/env_vars.sh
-
+    # automatically restore 
     echo 'export PERL5LIB='$PERL5LIB_BACKUP >> ./etc/conda/deactivate.d/env_vars.sh
     echo 'unset PERL5LIB_BACKUP' >> ./etc/conda/deactivate.d/env_vars.sh
+  else
+    echo 'no update'
 fi;
